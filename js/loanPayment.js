fb437a05f0302de149827ccb27a13276a6031e72
Dawson Schmitt <dischmitt@usf.edu>
2024-07-02 21:51:02 -0400

    Create loan payment calculation module.

--- js/loanPayment.js
+++ js/loanPayment.js
@@ -1,1 +1,3 @@
+//Task 1: Create this module
+//Task 3: Implement a module to calculate monthly loan payments using the formula: M = P[r(1+r)^n]/[(1+r)^n-1].
 export const interestRate = (P, n, r) => P[r(1+r)^n]/[(1+r)^n-1]
