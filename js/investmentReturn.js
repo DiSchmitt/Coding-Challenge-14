aa65691e7e9dcfc31c6f4f291b4966a17770c8c3
Dawson Schmitt <dischmitt@usf.edu>
2024-07-02 21:53:07 -0400

    Develop investment return calculation module.

--- js/investmentReturn.js
+++ js/investmentReturn.js
@@ -1,1 +1,4 @@
-const dividend = (A, P, r, n, t,) => A = P(1 + r/n)^(nt)
+//Task 1: Create this module
+//Task 4:Design a module to calculate the future value of an investment with the formula: A = P(1 + r/n)^(nt).
+//I had to slightly alter the formula so that the script would recognize 'nt' as being n*t and not an entirely new parameter.
+const dividend = (P, r, n, t,) => P(1 + r/n)^(n*t)
